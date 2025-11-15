431c09a44b5d6d8ae7e52d4e223454f9340e6f88
Lapin Matvey <lapinmatvej84@gmail.com>
2025-11-15 14:49:19 +0300

    выполнен первый этап

--- main.py
+++ main.py
@@ -13,21 +13,11 @@ def main() -> int:
         print(f"[ошибка конфигурации] {e}", file=sys.stderr)
         return 1
 
-    try:
-        deps = get_direct_dependencies(config.repo_url, config.package_name)
-    except ConfigError as e:
-        print(f"[ошибка данных репозитория] {e}", file=sys.stderr)
-        return 1
-    except Exception as e:
-        print(f"[неожиданная ошибка] {e}", file=sys.stderr)
-        return 1
-
-    print(f"Прямые зависимости пакета '{config.package_name}':")
-    if not deps:
-        print("(нет прямых зависимостей)")
-    else:
-        for dep in deps:
-            print(dep)
+    print(f"package_name={config.package_name}")
+    print(f"repo_url={config.repo_url}")
+    print(f"repo_mode={config.repo_mode}")
+    print(f"ascii_tree={config.ascii_tree}")
+    print(f"filter_substring={config.filter_substring}")
 
     return 0
 
